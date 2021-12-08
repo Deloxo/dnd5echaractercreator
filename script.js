2d6a79a4811b0946140802e3197cef4f8df2b527
Deloxo <37124397+Deloxo@users.noreply.github.com>
2021-12-07 23:24:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -84,149 +84,99 @@ function sumArray(array) {
             index = 0, // The iterator
             length = array.length, // Cache the array length
             sum = 0; // The total amount
-        index < length; // The "for"-loop condition
-        sum += array[index++] // Add number on each iteration
-    );
-    return sum;
-}
-
-function checkForRandom() {
-    points = 0;
-    for (x = 0; x < 6; x++) {
-        var statsdice = [Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1];
-        statsdice.sort();
-        console.log(statsdice);
-        statsdice.shift();
-        var result = sumArray(statsdice);
-        console.log(result);
-        switch (x) {
-            case 0:
-                stats.str = result;
-                break;
-            case 1:
-                stats.dex = result;
-                break;
-            case 2:
-                stats.con = result;
-                break;
-            case 3:
-                stats.int = result;
-                break;
-            case 4:
-                stats.wis = result;
-                break;
-            case 5:
-                stats.cha = result;
-                break;
-        }
+        index < length; // The "for" -loop condition sum +=array[index++] // Add number on each iteration ); return sum; } function checkForRandom() { points=0; for (x=0; x < 6; x++) { var statsdice=[Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1]; statsdice.sort(); console.log(statsdice); statsdice.shift(); var result=sumArray(statsdice); console.log(result); switch (x) { case 0: stats.str=result; break; case 1: stats.dex=result; break; case 2: stats.con=result; break; case 3: stats.int=result; break; case 4: stats.wis=result; break; case 5: stats.cha=result; break; } } document.getElementById("str").innerHTML=stats.str; document.getElementById("dex").innerHTML=stats.dex; document.getElementById("con").innerHTML=stats.con; document.getElementById("int").innerHTML=stats.int; document.getElementById("wis").innerHTML=stats.wis; document.getElementById("cha").innerHTML=stats.cha; } function changeStat(move) { switch (move) { case 'strUp' : if (points> 0) {
+        stats.str++; points--; document.getElementById("str").innerHTML = stats.str; document.getElementById("statbox").innerHTML = points;
     }
-    document.getElementById("str").innerHTML = stats.str;
-    document.getElementById("dex").innerHTML = stats.dex;
-    document.getElementById("con").innerHTML = stats.con;
-    document.getElementById("int").innerHTML = stats.int;
-    document.getElementById("wis").innerHTML = stats.wis;
-    document.getElementById("cha").innerHTML = stats.cha;
-}
-
-function changeStat(move) {
-    switch (move) {
-        case 'strUp':
-            if (points > 0) {
-                stats.str++;
-                points--;
-                document.getElementById("str").innerHTML = stats.str;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'strDown':
-            if (stats.str > 0) {
-                stats.str--;
-                points++;
-                document.getElementById("str").innerHTML = stats.str;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'dexUp':
-            if (points > 0) {
-                stats.dex++;
-                points--;
-                document.getElementById("dex").innerHTML = stats.dex;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'dexDown':
-            if (stats.dex > 0) {
-                stats.dex--;
-                points++;
-                document.getElementById("dex").innerHTML = stats.dex;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'conUp':
-            if (points > 0) {
-                stats.con++;
-                points--;
-                document.getElementById("con").innerHTML = stats.con;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'conDown':
-            if (stats.con > 0) {
-                stats.con--;
-                points++;
-                document.getElementById("con").innerHTML = stats.con;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'intUp':
-            if (points > 0) {
-                stats.int++;
-                points--;
-                document.getElementById("int").innerHTML = stats.int;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'intDown':
-            if (stats.int > 0) {
-                stats.int--;
-                points++;
-                document.getElementById("int").innerHTML = stats.int;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'wisUp':
-            if (points > 0) {
-                stats.wis++;
-                points--;
-                document.getElementById("wis").innerHTML = stats.wis;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'wisDown':
-            if (stats.wis > 0) {
-                stats.wis--;
-                points++;
-                document.getElementById("wis").innerHTML = stats.wis;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'chaUp':
-            if (points > 0) {
-                stats.cha++;
-                points--;
-                document.getElementById("cha").innerHTML = stats.cha;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
-        case 'chaDown':
-            if (stats.cha > 0) {
-                stats.cha--;
-                points++;
-                document.getElementById("cha").innerHTML = stats.cha;
-                document.getElementById("statbox").innerHTML = points;
-            }
-            break;
+    break;
+    case 'strDown':
+    if (stats.str > 0) {
+        stats.str--;
+        points++;
+        document.getElementById("str").innerHTML = stats.str;
+        document.getElementById("statbox").innerHTML = points;
     }
+    break;
+    case 'dexUp':
+    if (points > 0) {
+        stats.dex++;
+        points--;
+        document.getElementById("dex").innerHTML = stats.dex;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'dexDown':
+    if (stats.dex > 0) {
+        stats.dex--;
+        points++;
+        document.getElementById("dex").innerHTML = stats.dex;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'conUp':
+    if (points > 0) {
+        stats.con++;
+        points--;
+        document.getElementById("con").innerHTML = stats.con;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'conDown':
+    if (stats.con > 0) {
+        stats.con--;
+        points++;
+        document.getElementById("con").innerHTML = stats.con;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'intUp':
+    if (points > 0) {
+        stats.int++;
+        points--;
+        document.getElementById("int").innerHTML = stats.int;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'intDown':
+    if (stats.int > 0) {
+        stats.int--;
+        points++;
+        document.getElementById("int").innerHTML = stats.int;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'wisUp':
+    if (points > 0) {
+        stats.wis++;
+        points--;
+        document.getElementById("wis").innerHTML = stats.wis;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'wisDown':
+    if (stats.wis > 0) {
+        stats.wis--;
+        points++;
+        document.getElementById("wis").innerHTML = stats.wis;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'chaUp':
+    if (points > 0) {
+        stats.cha++;
+        points--;
+        document.getElementById("cha").innerHTML = stats.cha;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+    case 'chaDown':
+    if (stats.cha > 0) {
+        stats.cha--;
+        points++;
+        document.getElementById("cha").innerHTML = stats.cha;
+        document.getElementById("statbox").innerHTML = points;
+    }
+    break;
+}
 }
 
 var mode = 'points';
@@ -297,4 +247,131 @@ function pullAllStatsOut() {
     document.getElementsByClassName("statbox")[0].style.fontSize = "50px";
     document.getElementById("dumpButton").style.visibility = "visible";
     document.getElementsByClassName("statbox")[0].removeAttribute("onclick");
+}
+
+var extended = false;
+var optionmode = "";
+
+function chooseRace() {
+    if (extended == false) {
+        extended = true;
+        optionmode = "race";
+        document.getElementById("option0").style.visibility = "visible";
+        document.getElementById("option1").style.visibility = "visible";
+        document.getElementById("option2").style.visibility = "visible";
+        document.getElementById("option3").style.visibility = "visible";
+        document.getElementById("option4").style.visibility = "visible";
+        document.getElementById("option7").style.visibility = "visible";
+        document.getElementById("option8").style.visibility = "visible";
+        document.getElementById("option9").style.visibility = "visible";
+        document.getElementById("option10").style.visibility = "visible";
+        document.getElementById("option11").style.visibility = "visible";
+    } else if (extended == true) {
+        extended = false
+        optionmode = "";
+        document.getElementById("option0").style.visibility = "hidden";
+        document.getElementById("option1").style.visibility = "hidden";
+        document.getElementById("option2").style.visibility = "hidden";
+        document.getElementById("option3").style.visibility = "hidden";
+        document.getElementById("option4").style.visibility = "hidden";
+        document.getElementById("option5").style.visibility = "hidden";
+        document.getElementById("option6").style.visibility = "hidden";
+        document.getElementById("option7").style.visibility = "hidden";
+        document.getElementById("option8").style.visibility = "hidden";
+        document.getElementById("option9").style.visibility = "hidden";
+        document.getElementById("option10").style.visibility = "hidden";
+        document.getElementById("option11").style.visibility = "hidden";
+        document.getElementById("option12").style.visibility = "hidden";
+        document.getElementById("option13").style.visibility = "hidden";
+    }
+}
+
+function chooseClass() {
+    if (extended == false) {
+        extended = true;
+        optionmode = "class";
+        document.getElementById("option0").style.visibility = "visible";
+        document.getElementById("option1").style.visibility = "visible";
+        document.getElementById("option2").style.visibility = "visible";
+        document.getElementById("option3").style.visibility = "visible";
+        document.getElementById("option4").style.visibility = "visible";
+        document.getElementById("option5").style.visibility = "visible";
+        document.getElementById("option6").style.visibility = "visible";
+        document.getElementById("option7").style.visibility = "visible";
+        document.getElementById("option8").style.visibility = "visible";
+        document.getElementById("option9").style.visibility = "visible";
+        document.getElementById("option10").style.visibility = "visible";
+        document.getElementById("option11").style.visibility = "visible";
+        document.getElementById("option12").style.visibility = "visible";
+        document.getElementById("option13").style.visibility = "visible";
+    } else if (extended == true) {
+        extended = false;
+        optionmode = "";
+        document.getElementById("option0").style.visibility = "hidden";
+        document.getElementById("option1").style.visibility = "hidden";
+        document.getElementById("option2").style.visibility = "hidden";
+        document.getElementById("option3").style.visibility = "hidden";
+        document.getElementById("option4").style.visibility = "hidden";
+        document.getElementById("option5").style.visibility = "hidden";
+        document.getElementById("option6").style.visibility = "hidden";
+        document.getElementById("option7").style.visibility = "hidden";
+        document.getElementById("option8").style.visibility = "hidden";
+        document.getElementById("option9").style.visibility = "hidden";
+        document.getElementById("option10").style.visibility = "hidden";
+        document.getElementById("option11").style.visibility = "hidden";
+        document.getElementById("option12").style.visibility = "hidden";
+        document.getElementById("option13").style.visibility = "hidden";
+    }
+}
+
+function choose(option) {
+    document.getElementById("option0").style.visibility = "hidden";
+    document.getElementById("option1").style.visibility = "hidden";
+    document.getElementById("option2").style.visibility = "hidden";
+    document.getElementById("option3").style.visibility = "hidden";
+    document.getElementById("option4").style.visibility = "hidden";
+    document.getElementById("option5").style.visibility = "hidden";
+    document.getElementById("option6").style.visibility = "hidden";
+    document.getElementById("option7").style.visibility = "hidden";
+    document.getElementById("option8").style.visibility = "hidden";
+    document.getElementById("option9").style.visibility = "hidden";
+    document.getElementById("option10").style.visibility = "hidden";
+    document.getElementById("option11").style.visibility = "hidden";
+    document.getElementById("option12").style.visibility = "hidden";
+    document.getElementById("option13").style.visibility = "hidden";
+    if (optionmode = "class") {
+        switch (option) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+            case 12:
+            case 13:
+        }
+    }
+    if (optionmode = "race") {
+        switch (option) {
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 7:
+            case 8:
+            case 9:
+            case 10:
+            case 11:
+        }
+    }
+
+    extended = false;
+    optionmode = "";
 }
