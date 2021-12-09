d0557d38ef696d4c40c9b293a453ddd4e355b232
Deloxo <37124397+Deloxo@users.noreply.github.com>
2021-12-08 19:35:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -73,8 +73,8 @@ function checkForRandom() {
 
 function changeStat(move) {
   var statInd = (move + 1) / 2;
-  stats[statInd] += Math.Pow(-1, move + 1);
-  points -= Math.Pow(-1, move + 1);
+  stats[statInd] += Math.pow(-1, move + 1);
+  points -= Math.pow(-1, move + 1);
   document.getElementsByClassName(Object.keys(stats)[statInd]).innerHTML = stats[statInd];
   document.getElementById("statbox").innerHTML = points;
   showStats();
