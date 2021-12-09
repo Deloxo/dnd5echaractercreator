b0fae5823a078f9eaad8b7b2386393844cf06d39
Deloxo <37124397+Deloxo@users.noreply.github.com>
2021-12-08 21:17:39 -0500

    creating tooltips

--- script.js
+++ script.js
@@ -277,3 +277,12 @@ function sumArray(array) {
     );
     return sum;
 }
+
+document.addEventListener('mousemove', function(e) {
+  let body = document.querySelector('body');
+  let circle = document.getElementById('tooltip');
+  let left = e.offsetX;
+  let top = e.offsetY;
+  circle.style.left = left + 'px';
+  circle.style.top = top + 'px';
+});
